e70e8e96b7600c50ee6d025678ee6b16ca28725f
Hariharan <cvhariharan@protonmail.com>
2018-11-07 19:06:52 +0530

    Login function

--- js/client.js
+++ js/client.js
@@ -6,6 +6,8 @@ else {
     window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
 }
 
+const server = "http://localhost:3000/login";
+
 var keyphrase = createPhrase(32);
 document.getElementById("submitBtn").addEventListener('click', (event) => {
     event.preventDefault();
@@ -28,31 +30,57 @@ function createPhrase(n) {
   
     return phrase;
 }
-function sign(phrase){
+
+function sign(phrase, callback){
     //Actually submits the form after signing
     window.web3.eth.getAccounts((error, accounts) => {
-            window.web3.eth.sign(phrase, accounts[0], (err, sign) => {
-                if(err) {
-                    throw err;
-                }
-                console.log(sign);
-                // document.getElementById("address").value = accounts[0];
-                document.getElementById("sign").value = sign;
-                document.getElementById("phrase").value = keyphrase;
-                var form = document.getElementById("uploadForm");
-                form.submit();
-            });
+        if(error) {
+            throw error;
+        }
+        window.web3.eth.sign(phrase, accounts[0], (err, sign) => {
+            if(err) {
+                throw err;
+            }
+            callback(sign);
+            // document.getElementById("address").value = accounts[0];
+        });
     });
 }
+
+//For uploading news 
 function submit() {
     if(document.getElementById("title").value == "") {
         alert("Title cannot be empty");
     }
     else {
-        sign(toHex(keyphrase));
+        sign(toHex(keyphrase), function(sign) {
+            document.getElementById("sign").value = sign;
+            document.getElementById("phrase").value = keyphrase;
+            var form = document.getElementById("uploadForm");
+            form.submit();
+        });
     }
-    
 }
 
-
+//To be called when user signs up or logs in
+function login() {
+    const name = document.getElementById("name").value;
+    const bio = document.getElementById("bio").value;
+    console.log(name+" "+bio);
+    const keyphrase = createPhrase(32);
+    sign(toHex(keyphrase), function(sign) {
+        console.log("Sign: " + sign);
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("POST", server);
+        xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xhttp.send("name="+name+"&bio="+bio+"&sign="+sign+"&phrase="+keyphrase);
+        xhttp.onreadystatechange = function() {
+            if(xhttp.readyState ===4 && xhttp.status === 200) {
+                alert("Logged in!");
+            }
+            console.log(xhttp.responseText);
+        }
+        
+    });
+}
 
